596bedb7286d7a4f3bfa1fb795860076f76641e3
Rohan Yadav <83074738+rohany395@users.noreply.github.com>
2025-11-10 15:44:43 +0000

    initial setup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+pg=st.navigation([st.Page("./src/main.py",title="Neurogrnic Case Generator")])
+st.set_page_config(page_title="Case generator bot",)
+pg.run()
